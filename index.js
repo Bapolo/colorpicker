a7cea90b3b4e5bc5c5ecb283aab97cc86b8f608f
Jesus Pedro Bapolo <145621406+Bapolo@users.noreply.github.com>
2024-01-11 12:38:37 +0000

    update and add javascript feature

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 window.addEventListener("DOMContentLoaded", () =>
 {
-	console.log('Funcionou!');
+	const painel = document.querySelector("#painel");
+	const primeiraCor = document.querySelector("#primeiraCor");
+	const segundaCor = document.querySelector("#segundaCor");
+	const terceiraCor = document.querySelector("#terceiraCor");
+	const saidaRGB = document.querySelector("#valorRGB");
+	const btnCopiarRGB = document.querySelector("#copiarCodigo");
+
+	painel.style.backgroundColor = "red";
+
+	 primeiraCor.addEventListener("input", () =>
+	 
+	 	painel.style.backgroundColor = "rgb(" + `${primeiraCor.value}, ${segundaCor.value}, ${terceiraCor.value}` + ")"
+	) 	
+
+	 segundaCor.addEventListener("input", () =>
+	 
+	 	painel.style.backgroundColor = "rgb(" + `${primeiraCor.value}, ${segundaCor.value}, ${terceiraCor.value}` + ")"
+	) 	
+
+	 terceiraCor.addEventListener("input", () =>
+	 
+	 	painel.style.backgroundColor = "rgb(" + `${primeiraCor.value}, ${segundaCor.value}, ${terceiraCor.value}` + ")"
+	) 	
+	 
 })
