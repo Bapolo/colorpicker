1880f939e834b5fdd0ae8128f8ea0662bf1f3fc4
Jesus Pedro Bapolo <145621406+Bapolo@users.noreply.github.com>
2024-01-11 13:39:59 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -6,9 +6,11 @@ window.addEventListener("DOMContentLoaded", () =>
 	const terceiraCor = document.querySelector("#terceiraCor");
 	const saidaRGB = document.querySelector("#valorRGB");
 	const btnCopiarRGB = document.querySelector("#copiarCodigo");
+	const container = document.querySelector("#container");
+	const paragrafo = document.createElement("p");;
 
 	
-	saidaRGB.innerHTML = painel.style.backgroundColor
+	saidaRGB.innerHTML = "rgb(" + `${primeiraCor.value}, ${segundaCor.value}, ${terceiraCor.value}` + ")"
 
 	 primeiraCor.addEventListener("input", () => 
 	 	{
@@ -38,5 +40,51 @@ window.addEventListener("DOMContentLoaded", () =>
 	) 	
 
 
+	 btnCopiarRGB.addEventListener("click", () =>
+	 	 {
+	 	 	const valorRGB = "rgb(" + `${primeiraCor.value}, ${segundaCor.value}, ${terceiraCor.value}` + ")"
+
+	 	 	//O código a seguir foi criado pelo chatGPT
+
+	 	 	// Cria um elemento de texto temporário
+			const elementoTemporario = document.createElement("textarea");
+
+			// Define o valor do elemento de texto para o valor desejado
+			elementoTemporario.value = valorRGB;
+
+			// Adiciona o elemento temporário ao corpo do documento
+			document.body.appendChild(elementoTemporario);
+
+			// Seleciona o texto dentro do elemento de texto
+			elementoTemporario.select();
+
+			// Tenta copiar o texto para a área de transferência
+			document.execCommand("copy");
+
+			// Remove o elemento temporário do corpo do documento
+			document.body.removeChild(elementoTemporario);
+
+			// Exibe uma mensagem ou executa outra lógica após a cópia (opcional)
+			console.log("Valor copiado para a área de transferência: ", valorRGB);
+			setTimeout(mostrarNotificacaoNaTela, 500);
+			container.removeChild(paragrafo);
+			
+
+	 	 })
+
+	 function mostrarNotificacaoNaTela()
+	 {
+	 	
+	 	paragrafo.innerHTML = "<i class='fa-solid fa-check'></i> RGB copiado!";
+	 	paragrafo.style.color = "white";
+	 	paragrafo.style.fontSize = "1.2rem";
+	 	paragrafo.style.padding = "20px";
+	 	paragrafo.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+	 	paragrafo.style.borderRadius = "10px";
+	 	paragrafo.style.position = "absolute";
+	 	paragrafo.style.top = "40%";
+	 	container.appendChild(paragrafo) 
+	 	
+	 }
 	 
 })
